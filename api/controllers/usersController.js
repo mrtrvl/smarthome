b2ca842dcf61a79480a36b955f7a87d5ced4aa18
martti <mrt@mrt.ee>
2017-12-04 11:57:11 +0200

    User register method

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -5,6 +5,14 @@ const User = mongoose.model('User');
 
 exports.register = async (req, res) => {
     try {
+        let newUser = new User(req.body);
+        newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
+        let user = await newUser.save();
+        if (!user) {
+            throw new Error('Cannot save new user!');
+        }
+        user.hashPassword = undefined;
+        return res.status(201).json(user);
 
     } catch(err) {
         res.status(400).send(err.message);
